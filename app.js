114a090a8333cb9b68f8de41ff3da23c148ae471
deepak gupt <dg476504@gmail.com>
2024-04-13 17:33:04 +0530

    add the vercel file

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.status(200).json({
         masg: 'welcome to the app'
+    });
+});
 
-    })
-})
+// For handling all other routes
+app.all('*', (req, res) => {
+    res.status(404).json({
+        error: 'Route not found'
+    });
+});
 
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log("server is runnitn this port", port);
-})
+module.exports = app;
